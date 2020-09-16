763de158f877ba2c57917bf813f8454f197b1c20
Tsvetelina Borisova <ts.borisova3@gmail.com>
2020-09-16 18:10:21 +0300

    Add leaf - List

--- src/App.js
+++ src/App.js
@@ -35,18 +35,20 @@ function App() {
 
       <hr />
 
-      {
-        list.map(item =>
-          <div key={item.objectID}>
-            <span><a href={item.url}>{item.title}</a> </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
-        )
-      }
+      <List/>
     </div>
   );
+}
+
+function List(){
+  return list.map(item =>
+    <div key={item.objectID}>
+      <span><a href={item.url}>{item.title}</a> </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
+  )
 }
 
 export default App;

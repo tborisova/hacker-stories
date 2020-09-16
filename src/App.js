b15569620e152b152a18e58f666c9277df42f720
Tsvetelina Borisova <ts.borisova3@gmail.com>
2020-09-16 18:00:14 +0300

    lists

--- src/App.js
+++ src/App.js
@@ -2,23 +2,49 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const numbers = [1, 4, 9, 16];
+
+const newNumbers = numbers.map(function(number) {
+  return number * 2;
+});
+
+console.log(newNumbers);
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+
+      <hr />
+
+      {
+        list.map(item =>
+          <div key={item.objectID}>
+            <span><a href={item.url}>{item.title}</a> </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        )
+      }
     </div>
   );
 }

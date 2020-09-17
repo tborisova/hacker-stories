b2f21639a26532387c51cec72012b81812d9df79
Tsvetelina Borisova <ts.borisova3@gmail.com>
2020-09-17 11:57:54 +0300

    add props

--- src/App.js
+++ src/App.js
@@ -2,30 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const numbers = [1, 4, 9, 16];
-
-const newNumbers = numbers.map(function(number) {
-  return number * 2;
-});
-
-console.log(newNumbers);
-
 const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
@@ -34,7 +26,7 @@ const App = () => {
 
       <hr />
 
-      <List/>
+      <List list={stories}/>
     </div>
   );
 }
@@ -52,8 +44,8 @@ const Search = () => {
   )
 }
 
-const List = () =>
-  list.map(item =>
+const List = props =>
+  props.list.map(item =>
   <div key={item.objectID}>
     <span><a href={item.url}>{item.title}</a> </span>
     <span>{item.author}</span>

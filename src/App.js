cb8e79b438ddebe493694abe00eae4a1dbba17c2
Tsvetelina Borisova <ts.borisova3@gmail.com>
2020-09-17 10:34:37 +0300

    Use arrow functions

--- src/App.js
+++ src/App.js
@@ -25,38 +25,34 @@ const newNumbers = numbers.map(function(number) {
 
 console.log(newNumbers);
 
-function App() {
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
 
-      <Search/>
+    <Search/>
 
-      <hr />
+    <hr />
 
-      <List/>
-    </div>
-  );
-}
+    <List/>
+  </div>
+)
 
-function Search(){
-  return(
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-    </div>
-  )
-}
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div>
+)
 
-function List(){
-  return list.map(item =>
-    <div key={item.objectID}>
-      <span><a href={item.url}>{item.title}</a> </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-    </div>
-  )
-}
+const List = () =>
+  list.map(item =>
+  <div key={item.objectID}>
+    <span><a href={item.url}>{item.title}</a> </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </div>
+)
+
 
 export default App;
